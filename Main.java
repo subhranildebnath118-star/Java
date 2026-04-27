dd9025e83148d75da442b59ba32a4ca71c9fe25e
Subhranil Debnath <subhranildebnath118@gmail.com>
2026-04-27 16:00:40 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,48 @@
+
+interface Exam {
+    void percent_cal();
+}
+
+class Student {
+    String name;
+    int roll_no;
+    double marks1, marks2;
+
+    void getDetails(String n, int r, double m1, double m2) {
+        name = n;
+        roll_no = r;
+        marks1 = m1;
+        marks2 = m2;
+    }
+
+    void show() {
+        System.out.println("Name: " + name);
+        System.out.println("Roll No: " + roll_no);
+        System.out.println("Marks1: " + marks1);
+        System.out.println("Marks2: " + marks2);
+    }
+}
+
+class Result extends Student implements Exam {
+    double per;
+
+    public void percent_cal() {
+        per = (marks1 + marks2) / 2;
+    }
+
+    void display() {
+        show();
+        System.out.println("Percentage: " + per);
+    }
+}
+
+
+public class Main {
+    public static void main(String[] args) {
+        Result r = new Result();
+
+        r.getDetails("Subhranil", 4182, 80, 90);
+        r.percent_cal();
+        r.display();
+    }
+}
---
+++ pattern_two.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class pattern_two {
+    public static void main(String[] args) {
+           Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number of row and collumb");
+        int n= sc.nextInt();
+        for(int i = 1;i<=n;i++){
+            for(int j =1;j<=i;j++){
+                if(i%2!=0) {
+                    System.out.print(j+" ");
+                }else {
+                    System.out.print((char)(j+64)+(" "));
+                }
+            }
+            System.out.println();
+        }
+    }
+}
+
+    
+
